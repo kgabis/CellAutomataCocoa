42d8d8447d760beaa7e56cf33481c7daad821e3c
Krzysztof Gabis <kgabis@gmail.com>
2012-05-29 18:57:27 +0200

    Initial commit.

---
+++ PredatorPreyAutomata/MyCellGridView.h
@@ -0,0 +1,13 @@
+//
+//  CellGridView.h
+//  PredatorPreyAutomata
+//
+//  Created by Krzysztof Gabis on 29.05.2012.
+//  Copyright (c) 2012 AGH. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MyCellGridView : NSView
+
+@end
---
+++ PredatorPreyAutomata/MyCellGridView.m
@@ -0,0 +1,13 @@
+//
+//  CellGridView.m
+//  PredatorPreyAutomata
+//
+//  Created by Krzysztof Gabis on 29.05.2012.
+//  Copyright (c) 2012 AGH. All rights reserved.
+//
+
+#import "MyCellGridView.h"
+
+@implementation MyCellGridView
+
+@end
